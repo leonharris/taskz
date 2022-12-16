aa1be63d3cc82fc8d3a0ed101b99f8a614dcf17f
Leon Harris <mrleonharris@gmail.com>
2022-12-16 17:37:44 +0000

    added colour picker and bundled JS

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,14 +1,26 @@
-// @REF: https://stackoverflow.com/questions/60592825/sortablejs-isnt-saving-my-sorted-todo-list-in-localstorage
+// iro.js
+import iro from '@jaames/iro';
+// sortableJS
+import Sortable from 'sortablejs';
+
+/*
+* Iro Colour Picker
+* Used to set list colour
+*/
+var colorPicker = new iro.ColorPicker("#color-picker", {
+  // Set the size of the color color-picker
+  width: 300,
+  // Set the initial color to pure red
+  color: "#f00"
+});
 
 /*
 // SortableJS
 // Set initial sorting for Task lists on screen
 */
-
 function activateSortable() {
 	let taskListUL = document.querySelectorAll('.tasks-list');
-
-	for (i=0;i<taskListUL.length;i++) {
+	for (let i=0;i<taskListUL.length;i++) {
 		new Sortable(taskListUL[i], {
 			animation: 300,
 			group: 'task-list',
@@ -30,12 +42,16 @@ function activateSortable() {
 	};
 }
 
-// Add new List on form submit
+// Add new List Column on form submit
 
 function getStatusFormData(form) {
   let formData = new FormData(form);
   let blank_col =  true;
-  createList(blank_col, formData.get('column_title'), formData.get('column_color'));
+  let hex = colorPicker.color.hexString;
+  if (!hex) {
+	  hex = '#222';
+  }
+  createList(blank_col, formData.get('column_title'), hex);
 }
 document.getElementById("form--add-list").addEventListener("submit", function (event) {
   event.preventDefault();
@@ -243,3 +259,116 @@ function closeModal() {
 document.querySelector('.underlay').addEventListener('click', e => {
   document.body.classList.remove('show-modal')
 })
+
+
+
+
+/* localStorage JS
+---------------------------------------------------- */
+
+// Save page data to localStorage
+// Run function every x seconds
+const interval = setInterval(function() {
+   //console.log('run');
+   createLists_localStorage();
+}, 5000);
+
+
+function createLists_localStorage() {
+
+	let createLists_localStorage = [];
+
+	const col = document.querySelectorAll('.status-column');
+    let i = 1;
+	for (let list of col) {
+
+		//console.log(item.id); // column/list id
+		let list_id = list.id;
+		let col = document.getElementById(list_id);
+		let col_header_text = col.getElementsByTagName('header')[0].innerText;
+		// Get the --status-color style for column
+		var col_color = getComputedStyle(col).getPropertyValue('--status-color');
+
+		// get the tasks
+		let tasks = list.querySelectorAll('.task');
+
+		// loop over the tasks
+        // built the list tasks array
+		const task_data = [];
+        let ii = 1;
+		for (let task of tasks) {
+			let task_title_el = task.getElementsByClassName('task--title');
+			let task__title = task_title_el[0].innerHTML;
+			let task_content_el = task.getElementsByClassName('task--content');
+			let task__content = task_content_el[0].innerHTML;
+            //task_data.push('"' + ii + '": ["' + task__title + '","' + task__content + '"]');
+            task_data.push({
+                id: ii,
+                task_title: task__title,
+                task_content: task__content
+            });
+            ii++;
+		}
+        //console.log(task_data);
+
+        // build the array
+        createLists_localStorage.push({
+            name: col_header_text,
+            color: col_color,
+            tasks: task_data
+        });
+
+        i++;
+	}
+
+	// Put the object into storage
+	if (createLists_localStorage.length) {
+        //console.log(createLists_localStorage);
+        let jsonSetTasks = JSON.stringify(createLists_localStorage); // convert array to json
+		localStorage.setItem('tasks', jsonSetTasks);
+	}
+
+}
+
+function populateTasks() {
+
+    // Retrieve the Tasks object from storage
+	const retrievedTasks = localStorage.getItem('tasks');
+    let tasks = JSON.parse(retrievedTasks);
+    let i = 0;
+    for (let col of tasks) {
+
+        let col_name = col.name;
+        let col_color = col.color;
+        let task_items = col.tasks;
+
+        let blank_col = false;
+        createList(blank_col, col_name, col_color);
+
+        var listCol = document.getElementsByClassName('tasks-list');
+
+        for (let task of task_items) {
+
+            // Add task into list
+        	let task_li = createTask(task.task_title, task.task_content);
+        	listCol[i].appendChild(task_li);
+
+        }
+
+        i++;
+
+    }
+
+    // add the delete buttons after the board has been populated with content
+    // running it sooner means the lists don't exist at runtime
+    deleteList();
+    deleteTask();
+
+    // activate Sortabel JS on all task lists
+    activateSortable();
+
+}
+
+// build out the grid if items in localStorage
+// run on page load
+populateTasks();
