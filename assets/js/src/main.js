aac8be88f710203830dde9641992952b26001631
Leon Harris <mrleonharris@gmail.com>
2023-02-24 14:51:36 +0000

    visual update

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -181,7 +181,7 @@ function createList(blank_col, column_title, column_color) {
 	// Delete list button
 	let btn_delete_list = document.createElement("div");
 	btn_delete_list.classList.add('btn-delete-list-wrap');
-	btn_delete_list.innerHTML = '<button class="btn-action btn-delete-list"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0 0 114.6 0 256s114.6 256 256 256zm-72-280h144c13.3 0 24 10.7 24 24s-10.7 24-24 24H184c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg> Delete list</button>';
+	btn_delete_list.innerHTML = '<button class="btn-action btn-delete-list"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0 0 114.6 0 256s114.6 256 256 256zm-72-280h144c13.3 0 24 10.7 24 24s-10.7 24-24 24H184c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg> Delete category</button>';
 	column_footer.appendChild(btn_delete_list);
 
    	board.appendChild(column);
@@ -212,7 +212,7 @@ function deleteList() {
 	delete_list_btn.forEach(function(el) {
 		el.addEventListener('click', function(e) {
 			let parentList = el.closest('.status-column');
-			let confirmation = confirm("Are you sure you want to delete this list?");
+			let confirmation = confirm("Are you sure you want to delete this category?");
 		    if(confirmation == true){
 				parentList.remove();
 		    }
