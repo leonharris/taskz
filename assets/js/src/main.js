97475ae103aa34882b39829956ae7915f6c316a8
BBL <leonharris@users.noreply.github.com>
2026-02-19 10:44:51 +0000

    UI update

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -424,13 +424,21 @@ function createTask(task_title, task_content, task_priority) {
 	task_li.classList.add('task');
 	task_li.dataset.priority = priority;
 
-	// Priority badge
+	// Top row: priority badge + delete button
+	let task_top = document.createElement("div");
+	task_top.classList.add('task--top');
+
 	if (priority && priority !== "none") {
-		let priority_div = document.createElement("div");
-		priority_div.classList.add('task--tags');
-		priority_div.innerHTML = `<span class="task--tag task--tag-${priority}">${priority}</span>`;
-		task_li.appendChild(priority_div);
+		task_top.innerHTML = `<div class="task--tags"><span class="task--tag task--tag-${priority}">${priority}</span></div>`;
+	} else {
+		task_top.innerHTML = '<div class="task--tags"></div>';
 	}
+
+	let btn_delete_task = document.createElement("div");
+	btn_delete_task.classList.add('btn-delete-task-wrap');
+	btn_delete_task.innerHTML = '<button class="btn-action btn-delete-task"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0 0 114.6 0 256s114.6 256 256 256zm-72-280h144c13.3 0 24 10.7 24 24s-10.7 24-24 24H184c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg></button>';
+	task_top.appendChild(btn_delete_task);
+	task_li.appendChild(task_top);
 
 	// create task title
 	let task_header = document.createElement("h3");
@@ -445,12 +453,6 @@ function createTask(task_title, task_content, task_priority) {
 	let task_content_text = document.createTextNode(content_content);
 	task_content_div.appendChild(task_content_text);
 	task_li.appendChild(task_content_div);
-
-	// Delete task button
-	let btn_delete_task = document.createElement("div");
-	btn_delete_task.classList.add('btn-delete-task-wrap');
-	btn_delete_task.innerHTML = '<button class="btn-action btn-delete-task"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0 0 114.6 0 256s114.6 256 256 256zm-72-280h144c13.3 0 24 10.7 24 24s-10.7 24-24 24H184c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg></button>';
-	task_li.appendChild(btn_delete_task);
 
 	// return the <li class="task">
 	return task_li;
@@ -498,12 +500,14 @@ function createList(blank_col, column_title, column_color) {
 	header_text.contentEditable = "true";
 	header_text.addEventListener('input', markDirty);
 	column_header.appendChild(header_text);
-
+	
+	/*
 	// Edit list button
 	let btn_edit_list = document.createElement("button");
 	btn_edit_list.classList.add('btn-action', 'btn-edit-list');
 	btn_edit_list.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg>';
 	column_header.appendChild(btn_edit_list);
+	*/
 
 	// add the html into the list <header>
 	column_inner.appendChild(column_header);
